14bf11a2c9bd8b37c0e3829eef1b0f7658db2300
Cally Kilduff <callykilduff@gmail.com>
2024-01-21 20:08:57 +0000

    initial commit

---
+++ say_my_name.py
@@ -0,0 +1,9 @@
+# tell mac (unix style) users where the interpreter lives so that you can run this file from the terminal
+# !/usr/bin/python
+
+# create variables called firstname and surname
+# a variable is a placeholder in memory for data whose value can change
+firstname = 'Cally'
+surname = 'Kilduff'
+
+print(firstname, surname)
